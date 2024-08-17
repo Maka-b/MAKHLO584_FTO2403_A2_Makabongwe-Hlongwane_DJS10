d285e4c010702d4bc18994488500807f66923eec
Makabongwe Hlongwane <jabumak001@gmail.com>
2024-08-17 11:36:45 +0200

    list styling added and blogpost rendered

--- src/componets/BlogPosts.js
+++ src/componets/BlogPosts.js
@@ -3,17 +3,23 @@ import useFetch from "./customHooks/useFetch";
 
 export default function BlogPost() {
     const {data, error} = useFetch("https://jsonplaceholder.typicode.com/posts")
+
     
     if (error){
         return (<div className="error"><h1> Error: </h1>{error}</div>)
     }
     
+
     const Posts = () =>{
-        const posts = data.map(post=>{ return <li className="post" id={post.id}>
+        const listStyle = {
+                listStyle : "none"
+            }
+        
+        const posts = data.map(post=>{ return <li className="post" key={post.id}>
                                                   <h2> {post.title} </h2>
                                                   <p> {post.body} </p> 
                                                </li>})
-        return (<ul> {posts} </ul>)
+        return (<ul style={listStyle}> {posts} </ul>)
        
        }
     return(
